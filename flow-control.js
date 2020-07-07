96e25360f65825c516515f2406b8af048571cf85
Latisha <latisha_joe@yahoo.com>
2020-07-07 09:37:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -10,12 +10,12 @@ function teenager(age) {
   } else {return "You are not a teenager";}
 }
 
-function ageChecker(age) { 
+function ageChecker(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager!";
-  } else if (age <= 12) { 
+  } else if (age <= 12) {
   return "You are a kid";
-} else {return "You are a grownup";} 
+} else {return "You are a grownup";}
 }
 
 function ternaryTeenager(age) {
